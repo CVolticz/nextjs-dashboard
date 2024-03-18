a3cc2d7aa87460d5e39cc3183d2294d43e30736a
Ken Trinh <ktrinh.particle@gmail.com>
2024-03-17 21:33:47 -0400

    finishing up nextjs course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Customers | Acme Dashboard",
+}
+
+
 export default function Page() {
     return (
-        <p>Customera Page</p>
+        <p>Customers Page</p>
     );
 }
